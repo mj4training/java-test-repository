dd035f8144bbcbd5f3b2edbe4f988fc97ae3a8af
mj4training <mj4training@users.noreply.github.com>
2016-11-27 17:20:24 -0500

    Update App.java
    Added "there" to hello world message

--- src/main/java/com/ribeach/test/java_test/App.java
+++ src/main/java/com/ribeach/test/java_test/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello there World!" );
         System.out.println( "Goodbye World!" );
     }
 }
