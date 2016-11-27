5181c6c82e3439157bc54bac99ab834544bd271b
mj4training <mj4training@gmail.com>
2016-11-27 17:27:50 -0500

    Trying to merge again

--- src/main/java/com/ribeach/test/java_test/App.java
+++ src/main/java/com/ribeach/test/java_test/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello there World!" );
-        System.out.println( "Goodbye World!" );
+        //System.out.println( "Goodbye World!" );
     }
 }
